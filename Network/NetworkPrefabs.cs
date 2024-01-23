5a5bb1a1c64b26e565c7564cfae0314cf1db5401
Sara Jacquemin <sara@ily.li>
2024-01-23 14:37:23 +0100

    add network prefab utility

---
+++ Network/NetworkPrefabs.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Unity.Netcode;
+using UnityEngine;
+using UnityMDK;
+using System.Security.Cryptography;
+using System.Text;
+using LethalMDK.Logging;
+using UnityMDK.Reflection;
+
+namespace LethalMDK.Network;
+
+public static class NetworkPrefabs
+{
+    public static GameObject GetEmptyPrefab(string name)
+    {
+        var prefab = PrefabUtils.GetEmptyPrefab(name);
+        var networkObject = prefab.AddComponent<NetworkObject>();
+
+        var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(Assembly.GetCallingAssembly().GetName().Name + name));
+
+        networkObject.SetField("GlobalObjectIdHash", BitConverter.ToUInt32(hash, 0));
+        
+        RegisterNetworkPrefab(prefab);
+        return prefab;
+    }
+
+    public static void RegisterNetworkPrefab(GameObject prefab)
+    {
+        if (!NetworkManager.Singleton)
+        {
+            Log.Error("Network manager isn't ready");
+        }
+        
+        NetworkManager.Singleton.AddNetworkPrefab(prefab);
+    }
+}
