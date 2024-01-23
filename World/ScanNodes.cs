d558370eef13dd6e3937a1c081fff58aed3aad39
Sara Jacquemin <sara@ily.li>
2024-01-23 20:07:51 +0100

    quick fix

--- World/ScanNodes.cs
+++ World/ScanNodes.cs
@@ -15,6 +15,6 @@ public static class ScanNodes
     public static void AddScanNode<TComponent>(EType nodeType, string headerText, string subText, int minRange, int maxRange, Vector3? offset = null, bool requireLineOfSight = true, int creatureId = -1) where TComponent : Component
     {
         ScanNodeConstructor constructor = new(nodeType, headerText, subText, minRange, maxRange, offset ?? Vector3.zero, requireLineOfSight, creatureId);
-        SceneInjection.AddComponentInjector<TComponent>(constructor);
+        SceneInjection.AddComponentInjector<TComponent>(constructor, false);
     }
 }
