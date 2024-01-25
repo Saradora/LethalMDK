2a2fbfdc4d135b82a4f924c734e4773960716150
Sara Jacquemin <sara@ily.li>
2024-01-25 21:10:45 +0100

    networking tests

---
+++ Network/NetworkMessage.cs
@@ -0,0 +1,144 @@
+﻿using Unity.Collections;
+using Unity.Netcode;
+using UnityMDK.Logging;
+
+namespace LethalMDK.Network;
+
+public static class Messaging
+{
+    public static CustomMessagingManager Manager => NetworkManager.Singleton.CustomMessagingManager;
+
+    public static bool IsServerOrHost => NetworkManager.Singleton.IsServer || NetworkManager.Singleton.IsHost;
+
+    private static readonly List<ulong> _listWithoutHost = new();
+
+    public static ulong ServerClientId => NetworkManager.ServerClientId;
+
+    public static IReadOnlyList<ulong> ClientIds
+    {
+        get
+        {
+            var ids = NetworkManager.Singleton.ConnectedClientsIds;
+            _listWithoutHost.Clear();
+            ulong serverId = NetworkManager.ServerClientId;
+            foreach (var clientId in ids)
+            {
+                if (clientId == serverId)
+                {
+                    Log.Warning($"Skipping host: {clientId}");
+                    continue;
+                }
+                _listWithoutHost.Add(clientId);
+            }
+            return _listWithoutHost;
+        }
+    }
+}
+
+public class NetworkEvent
+{
+    public void SendMessage()
+    {
+        
+    }
+}
+
+public abstract class NetworkMessage<T> : IDisposable where T : unmanaged, IEquatable<T>
+{
+    protected readonly string messageName;
+
+    protected NetworkMessage(string name)
+    {
+        messageName = name;
+        if (!NetworkManager.Singleton)
+            throw new NullReferenceException("Network Singleton hasn't yet been instantied, you need to construct the NetworkMessage later.");
+        NetworkManager.Singleton.CustomMessagingManager.RegisterNamedMessageHandler(messageName, ReceiveMessage);
+    }
+
+    protected abstract void ReceiveMessage(ulong senderId, FastBufferReader messagePayload);
+
+    public void Dispose()
+    {
+        if (NetworkManager.Singleton && NetworkManager.Singleton.CustomMessagingManager is not null)
+            NetworkManager.Singleton.CustomMessagingManager.UnregisterNamedMessageHandler(messageName);
+    }
+}
+
+public class NetworkClientMessage<T> : NetworkMessage<T> where T : unmanaged, IEquatable<T>
+{
+    public NetworkClientMessage(string name) : base(name)
+    {
+    }
+
+    public event Action<T, ulong> MessageReceived;
+
+    protected override void ReceiveMessage(ulong senderId, FastBufferReader messagePayload)
+    {
+        if (!Messaging.IsServerOrHost) return;
+        
+        var receivedMessageContent = new ForceNetworkSerializeByMemcpy<T>(new T());
+        messagePayload.ReadValueSafe(out receivedMessageContent);
+        MessageReceived?.Invoke(receivedMessageContent.Value, senderId);
+    }
+
+    public void SendToServer(T value)
+    {
+        if (Messaging.IsServerOrHost) return;
+        
+        var messageContent = new ForceNetworkSerializeByMemcpy<T>(value);
+        var writer = new FastBufferWriter(FastBufferWriter.GetWriteSize<T>(), Allocator.Temp);
+        using (writer)
+        {
+            writer.WriteValueSafe(messageContent);
+            Log.Warning($"Send message: {value}");
+            NetworkManager.Singleton.CustomMessagingManager.SendNamedMessage(messageName, Messaging.ServerClientId, writer);
+        }
+    }
+}
+
+public class NetworkServerMessage<T> : NetworkMessage<T> where T : unmanaged, IEquatable<T>
+{
+    public NetworkServerMessage(string name) : base(name)
+    {
+    }
+
+    public event Action<T> MessageReceived;
+
+    protected override void ReceiveMessage(ulong senderId, FastBufferReader messagePayload)
+    {
+        if (Messaging.IsServerOrHost) return;
+        
+        var receivedMessageContent = new ForceNetworkSerializeByMemcpy<T>(new T());
+        messagePayload.ReadValueSafe(out receivedMessageContent);
+        MessageReceived?.Invoke(receivedMessageContent.Value);
+    }
+
+    public void SendToClient(T value, ulong clientId)
+    {
+        if (!Messaging.IsServerOrHost) return;
+        if (clientId == Messaging.ServerClientId) return;
+        
+        var messageContent = new ForceNetworkSerializeByMemcpy<T>(value);
+        var writer = new FastBufferWriter(FastBufferWriter.GetWriteSize<T>(), Allocator.Temp);
+        using (writer)
+        {
+            writer.WriteValueSafe(messageContent);
+            Log.Warning($"Send message: {value}");
+            NetworkManager.Singleton.CustomMessagingManager.SendNamedMessage(messageName, clientId, writer);
+        }
+    }
+
+    public void SendToAllClients(T value)
+    {
+        if (!Messaging.IsServerOrHost) return;
+        
+        var messageContent = new ForceNetworkSerializeByMemcpy<T>(value);
+        var writer = new FastBufferWriter(FastBufferWriter.GetWriteSize<T>(), Allocator.Temp);
+        using (writer)
+        {
+            writer.WriteValueSafe(messageContent);
+            Log.Warning($"Send message: {value}");
+            NetworkManager.Singleton.CustomMessagingManager.SendNamedMessage(messageName, Messaging.ClientIds, writer);
+        }
+    }
+}
---
+++ Network/test.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+using UnityMDK.Injection;
+using UnityMDK.Logging;
+
+namespace LethalMDK.Network;
+
+[InjectToComponent(typeof(RoundManager))]
+public class Test : MonoBehaviour
+{
+    [Tooltip("The name identifier used for this custom message handler.")]
+    public string MessageName = "MyCustomNamedMessage";
+
+    private NetworkClientMessage<Guid> _testMessageClient;
+    private NetworkServerMessage<Guid> _testMessageServer;
+
+    private void Awake()
+    {
+        _testMessageClient = new NetworkClientMessage<Guid>(MessageName);
+        _testMessageServer = new NetworkServerMessage<Guid>(MessageName);
+        
+        Log.Error($"Start wesh");
+
+        if (NetworkManager.Singleton.IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
+            _testMessageClient.MessageReceived += ReceiveMessageAsServer;
+        }
+        else
+        {
+            _testMessageServer.MessageReceived += ReceiveMessageAsClient;
+        }
+    }
+
+    private IEnumerator Start()
+    {
+        if (NetworkManager.Singleton.IsServer) yield break;
+        yield return new WaitForSeconds(2f);
+        _testMessageClient.SendToServer(Guid.NewGuid());
+    }
+
+    private void OnDestroy()
+    {
+        _testMessageClient.Dispose();
+        _testMessageServer.Dispose();
+        // Whether server or not, unregister this.
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientConnectedCallback;
+    }
+
+    private void OnClientConnectedCallback(ulong obj)
+    {
+        _testMessageServer.SendToAllClients(Guid.NewGuid());
+    }
+
+    private void ReceiveMessageAsClient(Guid guid)
+    {
+        Log.Error(guid);
+    }
+
+    private void ReceiveMessageAsServer(Guid guid, ulong clientId)
+    {
+        Log.Error(guid);
+    }
+}
